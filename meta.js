1096580b0737cd40bb6bb7eb4f772797cfab65fa
fmerizzi <fmerizzi@users.noreply.github.com>
2026-08-27 16:49:47 +0200

    Update latest forecast

---
+++ meta.js
@@ -0,0 +1,26 @@
+const forecastMeta = {
+  "date": "20260905",
+  "cycle": "18",
+  "leads": [
+    "f000",
+    "f006",
+    "f012",
+    "f024",
+    "f048"
+  ],
+  "grid": {
+    "projection": "webmercator",
+    "latMin": 20.188407909068292,
+    "latMax": 75.40311307316053,
+    "lonMin": -58.212350746268655,
+    "lonMax": 74.21235074626865,
+    "unit": "m/s",
+    "scale": 100.0,
+    "nodata": 65535
+  },
+  "tiles": {
+    "minZoom": 4,
+    "maxZoom": 6,
+    "tileSize": 256
+  }
+};
